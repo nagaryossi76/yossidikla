e078a918f37cfcf9957943cc70e3f6a74c1bd9a8
nagaryossi76 <nagaryossi76@gmail.com>
2025-06-14 22:51:17 +0000

    My First Program

--- backup.sh
+++ backup.sh
@@ -1,5 +1,43 @@
-#!bin/bash
+#!/bin/bash
 
-echo "**********MENU************"
+while true; do
+    echo "*********Select an option************:"
+    echo "1. Show the date"
+    echo "2. Show the time"
+    echo "3. Show the user name"
+    echo "4. Exit"
+    read -p "Enter your choice [1-4]: " choice
 
+    case $choice in
+        1)
+            echo "Current date: $(date +"%Y-%m-%d")"
+            ;;
+        2)
+            echo "Current time: $(date +"%H:%M:%S")"
+            ;;
+        3)
+            echo "User name: $USER"
+            ;;
+        4)
+            echo "Exiting. Goodbye!"
+            break
+            ;;
+        *)
+            echo "Invalid option. Please choose 1-4."
+            ;;
+    esac
+    echo
+done
 
+B
+B
+A
+A
+A
+A
+A
+A
+A
+A
+A
+done 
