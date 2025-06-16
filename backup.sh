8c89ab26a038a5f0964bc2de52743567dd8a47bb
Yossi Nagar <nagaryossi76@gmail.com>
2025-06-17 01:33:15 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
+
+current_date=$(date +%Y-%m-%d)
+current_time=$(date +"%H:%M")
+
 echo "**********Create a backup of a folder at a date and time you choose**********"
 # Prompt user to enter the start date (format: YYYY-MM-DD)
 echo "Please enter The Start date (format: YYYY-MM-DD):"
 read startdate
-# Prompt user to enter the start time (format: HH:MM)
-echo "Please enter The Start time (format: HH:MM):"
-read starttime
-
-echo "Your Start Date is : $startdate"
-echo "Your Start time is : $starttime"
-
-current_date=$(date +%Y-%m-%d)
-current_time=$(date +"%H:%M")
 if [[ "$startdate" > "$current_date" ]]; then
-    echo "Today is $current_date and The Time is $current_time"
+# Prompt user to enter the start time (format: HH:MM)
+    echo "Please enter The Start time (format: HH:MM):"
+    read starttime
+    echo "Your Start Date is : $startdate"
+    echo "Your Start time is : $starttime"
+
 else
     echo "Incorrect due date"
 fi
