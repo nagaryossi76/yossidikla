7b7e7584995be39e11905f1e1899c0d2a607eaed
Yossi Nagar <nagaryossi76@gmail.com>
2025-06-17 00:02:41 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "**********Create a backup of a folder at a date and time you choose**********"
+# Prompt user to enter a date and time (format: YYYY-MM-DD HH:MM)
+echo "Please enter a date and time (format: YYYY-MM-DD HH:MM):"
+read datetime
+
 # Define the backup directory path
 DESKTOP_DIR="$HOME/Desktop/BACKUP"
 
