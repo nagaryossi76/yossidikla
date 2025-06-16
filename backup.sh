90eb42ff17fe6014fe561305c50b269386d78fb7
Yossi Nagar <nagaryossi76@gmail.com>
2025-06-16 23:55:20 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Define the backup directory path
 DESKTOP_DIR="$HOME/Desktop/BACKUP"
 
+# Check if the directory does not exist
 if [ ! -d "$DESKTOP_DIR" ]; then
-    echo "יוצר תיקיית גיבוי ב-$DESKTOP_DIR..."
+    echo "Creating a backup directory at HOME/Desktop/BACKUP "
     mkdir -p "$DESKTOP_DIR"
 else
-    echo "התיקייה כבר קיימת"
+    echo "The directory already exists"
 fi
