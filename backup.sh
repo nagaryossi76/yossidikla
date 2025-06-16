f0ec54364ae99077632b5de51f3f0580f787064b
Yossi Nagar <nagaryossi76@gmail.com>
2025-06-17 01:19:17 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -7,9 +7,9 @@ read startdatetime
 
 echo "Your Start Date&Time is : $startdatetime"
 TODAY=$(date +%Y-%m-%d)
-TIME= $(date +%H:%M:%S)
+current_time=$(date +"%H:%M:%S")
 
-echo "Today is $TODAY and The Time is $TIME"
+echo "Today is $TODAY and The Time is $current_time"
 
 # Define the backup directory path
 DESKTOP_DIR="$HOME/Desktop/BACKUP"
